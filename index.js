2545831b378b617b0cf32ce45a3fa75db0959ed5
shravan1998 <shrkum51@gmail.com>
2019-11-15 22:54:18 +0530

    Added mp3 file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var cors = require("cors");
 
 
 
-var db = mongo.connect("localhost:27017/",function(err,response){
+var db = mongo.connect("mongodb://localhost:27017/musicstreamer",function(err,response){
     if(err){
         throw err;
     }else{
@@ -18,46 +18,21 @@ var db = mongo.connect("localhost:27017/",function(err,response){
 var app = express();
 var router = express.Router();
 app.use(bodyParser());
-app.use(bodyParser.json({limit:'5mb'}));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
 app.use(function(req,res,next){
-    res.setHeader('Access-Control-Allow-Origin','https://cs-dept-scem.netlify.com');
+    res.setHeader('Access-Control-Allow-Origin','http://localhost:4200');
     res.setHeader('Allow-Access-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
     res.setHeader('Allow-Access-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept');
     res.setHeader("Access-Control-Allow-Credentials",true);
     next();
 });
-var Schema = mongo.Schema;
 
 
-//var model = mongo.model('achievements',achievements);
-/*app.get("/",function(req,res) {
-    model.find({})
-    .exec(function(err,data){
-        if(err){
-            res.send(err);
-        }else{
-            res.send(data);
-            console.log(data);
-            
-        }
-    })
-});
-app.post("/",function(req,res){
-    console.log(req.body);
-    var mod = new model(req.body);
-    mod.save(function(err,data){
-        if(err){
-            res.send(err);
-        }
-        else{
-            res.send({data:"Record Inserted"});
-            console.log(data);
-        }
-    });
-});*/
+
+
 
 var port = 8000;
 
